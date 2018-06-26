1949b2c6643d9c4664ed3419324f5db41999d07b
Andreas Baumbach <healther.astro@gmail.com>
2018-06-27 00:06:41 +0200

    add more benchmarks

--- mapping/networks/ising/plot.py
+++ mapping/networks/ising/plot.py
@@ -12,21 +12,23 @@ from collections import defaultdict
 
 plotdata = defaultdict(list)
 for jsfile in glob.glob('*.json'):
-	if jsfile.startswith("benchmarks"):
-		continue
-	name, parameters = jsfile.split("_network_")
-	with open(jsfile, 'r') as f:
-		data = json.load(f)
-		nneurons = data['results'][3]['value']
-		totsyn = data['results'][2]['value']
-		losssyn = data['results'][4]['value']
-		lossl1syn = data['results'][5]['value']
-		plotdata[name].append([nneurons, losssyn/float(totsyn), lossl1syn/float(totsyn)])
+    if jsfile.startswith("benchmarks"):
+        continue
+    name, parameters = jsfile.split("_network_")
+    with open(jsfile, 'r') as f:
+        data = json.load(f)
+        nneurons = data['results'][3]['value']
+        totsyn = data['results'][2]['value']
+        losssyn = data['results'][4]['value']
+        lossl1syn = data['results'][5]['value']
+        plotdata[name].append([nneurons, losssyn/float(totsyn), lossl1syn/float(totsyn)])
 
 for name in plotdata:
-	pd = np.array(plotdata[name])
+    pd = np.array(plotdata[name])
 
-	plt.plot(pd[:,0], pd[:, 1], 'x', label='synapseloss')
-	plt.plot(pd[:,0], pd[:, 2], 'x', label='synapsel1loss')
-	plt.legend()
-	plt.savefig('{}_loss.pdf'.format(name))
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(pd[:,0], pd[:, 1], 'x', label='synapseloss')
+    ax.plot(pd[:,0], pd[:, 2], 'x', label='synapsel1loss')
+    ax.legend()
+    plt.savefig('{}_loss.pdf'.format(name))

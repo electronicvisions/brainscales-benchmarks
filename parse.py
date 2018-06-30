7b4ee5cf706c5a44b10ac35665a935735a66ad60
Andreas Baumbach <andreas.baumbach@kip.uni-heidelberg.de>
2018-06-30 17:57:38 +0200

    add multiprocessing to parallelise mapping runs
    Change-Id: I3f22952f74b16c3c1cfb8968aaa0cccd08e4c46d

--- parse.py
+++ parse.py
@@ -1,10 +1,47 @@
 #!/usr/bin/env python
 
-import json
-import subprocess
+import argparse
 import itertools as it
+import json
+import multiprocessing as mp
+import subprocess
+
+def run(args, useslurm=True):
+    """The args tuple needs to be formatted as follows:
+    args[0]: tuple of values for the arguments
+    args[1]: tuple of command line arguments
+    args[2]: basecommand to call on the commandline with above arguments
+    args[3]: name of the mapping problem"""
+    argtuple, argnames, basecommand, name = args
+    argstr = " "
+    for argname, argvalue in zip(argnames, argtuple):
+        argstr += "{} {} ".format(argname, argvalue)
+    command = basecommand + argstr + "--name {}".format(name)
+    print(command)
+    if useslurm:
+        subprocess.check_call(["srun", "-p", "jenkins", "python"] + command.split(" "))
+    else:
+        subprocess.check_call(["python"] + command.split(" "))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--multiprocessing', action='store_true', default=False)
+parser.add_argument('--processes', default=20, type=int,
+                    help='Number of processes to spawn slurm jobs with, should'
+                        ' be larger than number of concurrent jenkins'
+                        ' slurm-jobs (at time of writing: 6). An increased'
+                        ' number does not particularly hurt, as the processes'
+                        ' will be blocked by the srun call anyways.')
+args = parser.parse_args()
 
 benchmarks = json.load(open("benchmarks.json", "r"))
+
+if args.multiprocessing:
+    # as the pool only holds processes waiting for srun to return
+    # the number is not particularly critical, should be larger than
+    # number of available slurm jobs
+    pool = mp.Pool(processes=args.processes)
+
+argtuples = []
 
 for item in benchmarks:
     name = item["model"]["name"]
@@ -15,11 +52,14 @@ for item in benchmarks:
     for argumentname, argumentvalues in arguments.items():
         argnames.append(argumentname)
         argvalues.append(argumentvalues)
-    for argtuple in it.product(*argvalues):
-        argstr = " "
-        for i, arg in enumerate(argtuple):
-            argstr += "{} {} ".format(argnames[i], arg)
-        command = basecommand + argstr + "--name {}".format(name)
-        print(command)
-        # TODO: make parallel and option to use w/out slurm
-        subprocess.check_call(["srun", "-p", "jenkins", "python"] + command.split(" "))
+
+    if args.multiprocessing:
+        argtuples += [(at, argnames, basecommand, name) for at in it.product(*argvalues)]
+    else:
+        for argtuple in it.product(*argvalues):
+            run(argtuple, argnames, basecommand, name, False)
+
+if args.multiprocessing:
+    pool.map(run, argtuples)
+    pool.close()
+

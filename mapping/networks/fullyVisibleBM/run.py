7383888513f5df6a7d8371c33be2748da5b82d37
Akos Kungl <fkungl@kip.uni-heidelberg.de>
2018-07-03 13:54:22 +0200

    fully visible BM model added
    Change-Id: If406545839b6456a990ba2e1668b80434c4ad9f4

---
+++ mapping/networks/fullyVisibleBM/run.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+import argparse
+from datetime import datetime
+import distutils.util
+import json
+
+import pyhmf as pynn
+import pymarocco
+
+from pysthal.command_line_util import init_logger
+init_logger("WARN", [])
+
+class fullyVisibleBmNetwork(object):
+    def __init__(self, N, marocco, model=pynn.EIF_cond_exp_isfa_ista):
+        self.N = N
+        self.model = model
+        self.marocco = marocco
+
+        pynn.setup(marocco=self.marocco)
+
+    def build(self):
+
+        # Set the neurons
+        self.neurons = pynn.Population(self.N, self.model)
+
+        # in the fully visible BM there each neuron projects to each neuron
+        # both inhibitory and excitatory to enable switching the sing of the
+        # connection during eventual training
+        # self connections are excluded
+        # This model only sets the skeleton of the BM without the noise sources
+        connector = pynn.AllToAllConnector(weights=0.003,
+                                           allow_self_connections=False)
+        projExc = pynn.Projection(self.neurons,
+                        self.neurons,
+                        connector,
+                        target='excitatory')
+        projInh = pynn.Projection(self.neurons,
+                        self.neurons,
+                        connector,
+                        target='inhibitory')
+
+    def run(self):
+        pynn.run(1)
+        pynn.end()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--N',  default=5000, type=int)
+    parser.add_argument('--shuffle_switches', dest='shuffle_switches',
+                        type=lambda x:bool(distutils.util.strtobool(x)),
+                        default='false')
+    parser.add_argument('--name',  default="fullyVisibleBm_network", type=str)
+
+    args = parser.parse_args()
+
+    taskname = "N{}_s{}".format(args.N, args.shuffle_switches)
+
+    marocco = pymarocco.PyMarocco()
+    marocco.continue_despite_synapse_loss = True
+    marocco.calib_backend = pymarocco.PyMarocco.CalibBackend.Default
+    marocco.calib_path = "/wang/data/calibration/brainscales/default"
+    marocco.defects_path = "/wang/data/calibration/brainscales/default"
+    marocco.l1_routing.shuffle_switches(args.shuffle_switches)
+    marocco.persist = "results_{}_{}.xml.gz".format(args.name, taskname)
+
+    start = datetime.now()
+    r = fullyVisibleBmNetwork(args.N, marocco)
+    r.build()
+    mid = datetime.now()
+    try:
+        r.run()
+        totsynapses = marocco.stats.getSynapses()
+        totneurons = marocco.stats.getNumNeurons()
+        lostsynapses = marocco.stats.getSynapseLoss()
+        lostsynapsesl1 = marocco.stats.getSynapseLossAfterL1Routing()
+    except RuntimeError:
+        # couldn't place all populations
+        totsynapses = 1
+        totneurons = 1
+        lostsynapses = 1
+        lostsynapsesl1 = 1
+
+    end = datetime.now()
+
+    result = {
+        "model" : args.name,
+        "task" : taskname,
+        "timestamp" : datetime.now().isoformat(),
+        "results" : [
+            {"type" : "performance",
+             "name" : "setup_time",
+             "value" : (end-mid).total_seconds(),
+             "units" : "s",
+             "measure" : "time"
+         },
+            {"type" : "performance",
+             "name" : "total_time",
+             "value" : (end-start).total_seconds(),
+             "units" : "s",
+             "measure" : "time"
+         },
+            {"type" : "performance",
+             "name" : "synapses",
+             "value" : totsynapses
+         },
+            {"type" : "performance",
+             "name" : "neurons",
+             "value" : totneurons
+         },
+            {"type" : "performance",
+             "name" : "synapse_loss",
+             "value" : lostsynapses
+         },
+            {"type" : "performance",
+             "name" : "synapse_loss_after_l1",
+             "value" : lostsynapsesl1
+         }
+        ]
+    }
+
+    with open("{}_{}_results.json".format(result["model"], result["task"]), 'w') as outfile:
+        json.dump(result, outfile)
+
+if __name__ == '__main__':
+    main()
